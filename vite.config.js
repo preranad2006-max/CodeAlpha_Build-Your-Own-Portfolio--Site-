7fb5100aa65a918ccd8d960e902e65017a5b2c4f
prerana d <preranad2006@gmail.com>
2026-09-14 18:06:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    allowedHosts: true,
-  },
+  plugins: [react()],
+  base: "/CodeAlpha_Build-Your-Own-Portfolio--Site-/",
 });
